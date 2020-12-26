bc484e5f3f0a6cddc98d979194e4ee471c57640a
Michael Brooks <michaelbrooks100@yahoo.co.uk>
2020-12-26 16:36:28 +0000

    BOnes of the header in placeCommit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <Header/>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/services">Services</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
 
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "@/components/Header.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Header
+  }
+})
+export default class App extends Vue {}
+</script>
+
+
+
 <style lang="scss">
+
+
 #app {
+  overflow-y: auto;
+	height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
