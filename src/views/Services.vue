5a1a72fb918f5d4d82a63e6c3ae4a9401456af90
Michael Brooks <michaelbrooks100@yahoo.co.uk>
2020-12-27 19:10:27 +0000

    Services list added

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,22 @@
 <template>
-<div>Services</div>
+<!-- <div class="hr">&nbsp;</div> -->
+<div class="indentedContent"> 
+        <h1 class="h1">
+                List of our services:
+        </h1>
+        <ul class="mainList">
+                <li class="serviceItem">Hand Railings / Safety railings</li>
+                <li class="serviceItem"> Ornamental gates</li>
+                <li class="serviceItem">All Steel work including stainless steel and aluminium work</li>
+                <li class="serviceItem">Truck trailer repair</li>
+                <li class="serviceItem">Car, Van and jeep and body and chassis repair</li>
+                <li class="serviceItem">Car Trailer, Livestock trailer, horsebox and plant trailer repair and service</li>
+        </ul>
+        <p class="paragraph_Main">
+                Please feel free to contact us by phone or email to discuss your ideas,
+                plans and requirements or just an informal chat about what we can deliver.
+        </p>
+</div>
 </template>
 
 <script lang="ts">
@@ -13,5 +30,6 @@ export default class Services extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+// @import '../styles/demo.css';
 
 </style>
