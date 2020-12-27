3586c0f97cb3a5656c707e014ac9a1531ffd2451
Michael Brooks <michaelbrooks100@yahoo.co.uk>
2020-12-27 18:32:45 +0000

    Added Main Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="imageContainer">
+        <img class="rounded" src="../assets/Drill.jpg" width="100%" />
+    </div>
+    <div class="indentedContent">
+            <h1 class="h1">What we do</h1>
+            <p class="paragraph_Main">
+                    We are a leading Specialist Engineering Metal Fabrication company located outside Ennis in Co. Clare.
+                    <a class="googleMapsAnchor" href="https://maps.app.goo.gl/YeKn7usWHGaX4GuF6">
+                            Click to View on Google Maps</a><br>
+                    We specialise in safety hand railings, decorative gates and truck/van/ car body repairs. We also
+                    provide a general wedding service.<br>
+            </p>
+            <p>
+                    Take a look at "Our Work" page to see a small sample of our prior work.
+                    Tom is a fully CAD certified
+            </p>
+            <div class="divider"></div>
+    </div>
+    <div class="indentedContent">
+            <h1 class="h1">How can we help you?</h1>
+            <p class="paragraph_Main">
+                    We work closely with new and existing customers to deliver workable solutions to produce great results.
+
+                    Contact us today for a free quotation.
+
+            </p>
+            <div class="divider"></div>
+            <p class="paragraph_Main">
+
+            </p>
+    </div>
   </div>
 </template>
 
@@ -15,3 +45,8 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+@import '../styles/demo.css';
+
+</style>
