bde3fd76e709e1e4016feea5985c0c1a8c2bd74f
Michael Brooks <michaelbrooks100@yahoo.co.uk>
2020-12-27 18:28:04 +0000

    Added Contact Page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,43 @@
 <template>
-<div>Contact</div>
+<div class="indentedContent">
+    <div class="paragraph_Main">
+        <h1 class="h1">Contact Information</h1>
+
+        <p><strong>Our Opening Hours:</strong></p>
+        <ul>
+            <li><strong>Monday to Thursday: </strong>8.00 AM to 5.30 PM</li>
+            <li><strong>Friday: </strong>8.00 AM to 4.30 PM</li>
+        </ul>
+        <div class="divider"></div>
+        <p><strong>Contact:</strong></p>
+        <div>
+            <div class="bulletLess"><i class="fa fa-phone" aria-hidden="true"></i><a class="contactIcon" href="tel:+353879554650">087 955 4650</a></div>
+            <div class="bulletLess">
+                <i class="fa fa-envelope" aria-hidden="true"></i><a id="email" class="contactIcon" href="mailto:tombrooksengineering@gmail.com">tombrooksengineering@gmail.com</a>
+                <input type="image" class="clickable" src="img/copyIcon.png" title="Click to Copy Email"
+                    onclick="copyFromSpanElementToClipboard('email')" width="24" height="24">
+            </div>
+        </div>
+        <div style="margin-top: 20px;" class="divider"></div>
+        <div>
+            <p><strong>Address:</strong></p>
+            <span>Drumcaurin</span>
+            <br />
+            <span>Ennis</span>
+            <br />
+            <span>Co. Clare</span>
+            <br />
+            <span>V95 XN3P</span>
+            <br />
+            <br />
+            <a class="googleMapsAnchor"
+                href="https://maps.app.goo.gl/YeKn7usWHGaX4GuF6">
+                View on Google Maps
+            </a>
+        </div>
+
+    </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -13,5 +51,5 @@ export default class Contact extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import '../styles/demo.css';
 </style>
